88ac23d4d05ae02f997632e1beac5eb404fd1e73
darlanZero <darliankeira229@gmail.com>
2024-03-25 22:26:23 -0300

    fixing code lens

--- Web/src/components/shared/Footer.tsx
+++ Web/src/components/shared/Footer.tsx
@@ -4,31 +4,31 @@ import { Link } from "react-router-dom"
 
 
 const Footer = () => {
-  return (
-    <footer
+    return (
+        <footer
           className="flex flex-row w-full justify-between bg-dark-theme-secondary py-4 px-8 gap-4"
         >
-          <p
-            className='text-xs text-slate-200'
-          >
-            <CopyrightCircleFilled />
-            2024 StarsFord. All rights reserved
-          </p>
-
-          <Cloudy color="purple" width={50} height={20} />
-
-          <p
-              className="text-sm ml-2"
+            <p
+                className='text-xs text-slate-200'
             >
-              <Link
-                to='/Rules'
-                className="underline underline-offset-2 text-slate-200 hover:text-cyan-400"
-              >
-                Terms & Conditions.
-              </Link> 
-          </p>
-      </footer>
-  )
+                <CopyrightCircleFilled />
+                2024 StarsFord. All rights reserved
+            </p>
+
+            <Cloudy color="purple" width={50} height={20} />
+
+            <p
+                className="text-sm ml-2"
+                >
+                <Link
+                    to='/Rules'
+                    className="underline underline-offset-2 text-slate-200 hover:text-cyan-400"
+                >
+                    Terms & Conditions.
+                </Link> 
+            </p>
+        </footer>
+    )
 }
 
 export default Footer
